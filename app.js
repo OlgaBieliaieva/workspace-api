bdfb49154a3e9b1cf705dafd3cb22fde6a07c4c2
OlgaBieliaieva <belyaevaolya0@gmail.com>
2025-02-06 08:51:32 +0200

    added CORS configs

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(morgan("tiny"));
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
