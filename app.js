68951f97b60f3a5193fdaa95b6187b65dca1123e
Olha Shapoval <belyaevaolya0@gmail.com>
2025-11-12 15:12:46 +0200

    added app.options

--- app.js
+++ app.js
@@ -29,6 +29,19 @@ app.use(
     credentials: true,
   })
 );
+app.options(
+  "*",
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 
